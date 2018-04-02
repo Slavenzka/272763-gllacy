15deed83f4daabf32d46848c6318874a27a2d3e8
Slavenzka <proekt.energetika@gmail.com>
2018-04-02 18:03:22 +0300

    Исправил замечания проверяющего

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 var open = document.querySelector(".btn-feedback");
-    var modal = document.querySelector(".feedback-form-wrapper");
-    var close = modal.querySelector(".feedback__btn-close");
-    var userName = modal.querySelector("[name=feedback-name]");
-    var userMail = modal.querySelector("[name=feedback-email]");
-    var form = modal.querySelector(".feedback-form__content");
+var modal = document.querySelector(".feedback-form-wrapper");
+var close = modal.querySelector(".feedback__btn-close");
+var userName = modal.querySelector("[name=feedback-name]");
+var userMail = modal.querySelector("[name=feedback-email]");
+var form = modal.querySelector(".feedback-form__content");
+var popup = modal.querySelector(".feedback-form");
     
     open.addEventListener("click", function(evt) {
       evt.preventDefault();
@@ -14,11 +15,14 @@ var open = document.querySelector(".btn-feedback");
     close.addEventListener("click", function(evt) {
       evt.preventDefault();
       modal.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     });
     
     form.addEventListener("submit", function(evt) {
       if (!userName.value || !userMail.value) {
       evt.preventDefault();
-      console.log("¬ведите им€ и почту!");
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
       }
     });
